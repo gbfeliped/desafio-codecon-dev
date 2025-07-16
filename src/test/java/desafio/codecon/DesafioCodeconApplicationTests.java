f6588f7122a5485ef1d8a1e1afa33b2b4ce65706
Gabriel F. <44969335+gbfeliped@users.noreply.github.com>
2025-07-16 09:58:59 -0300

    init project

---
+++ src/test/java/desafio/codecon/DesafioCodeconApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio.codecon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCodeconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
